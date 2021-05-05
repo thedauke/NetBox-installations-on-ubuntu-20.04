d68ccae8c86e24e27e25c92bc985daaeaa8d0dcd
TDADS <thedauke@gmail.com>
2021-05-05 11:41:13 +0600

    Fix usless script (

--- NetBox_installation_part_1_of_2.sh
+++ NetBox_installation_part_1_of_2.sh
@@ -2,8 +2,9 @@
 #Tutorial to install docker with netbox on Ubuntu 20.04 by tdads
 
 sudo apt-get update && sudo apt-get upgrade -y
-sudo apt-get install docker git curl tree python3-pip -y
+sudo apt-get install git curl python3-pip -y
 sudo pip3 install docker-compose
+sudo snap install docker
 
        #if pip doesn't works
       
@@ -16,7 +17,7 @@ mkdir -p ~/NetBox && cd ~/NetBox
 git clone -b release https://github.com/netbox-community/netbox-docker.git netbox-docker
 cd netbox-docker 
 
-tree docker-compose.override.yml <<EOF
+tee docker-compose.override.yml <<EOF
 version: '3.4'
 services:
   netbox:
@@ -29,3 +30,6 @@ sudo docker-compose up -d
 sudo docker-compose stop
 service docker stop
 sudo docker-compose up -d
+
+#TEST
+# sudo IMAGE=netboxcommunity/netbox:latest ./test.sh
